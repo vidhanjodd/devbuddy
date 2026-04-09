d6399af8cfbe1157d950f7dcabf5d654e6ef90b1
Vidhan <vidhanlonare@gmail.com>
2026-04-09 14:10:14 +0530

    first commit

---
+++ src/main/java/com/vidhan/devbuddy/DevbuddyApplication.java
@@ -0,0 +1,13 @@
+package com.vidhan.devbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevbuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vidhan/devbuddy/DevbuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidhan.devbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevbuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
